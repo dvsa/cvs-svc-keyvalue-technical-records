23dbfbc13ded65355821835aa7a13b996f2078fc
Steven.Pears <steven.pears@live.co.uk>
2022-09-09 10:44:22 +0100

    remove source but keep setup

--- src/domain/Processors/index.ts
+++
@@ -1,7 +0,0 @@
-export * from "./VehicleProcessor";
-export * from "./CarProcessor";
-export * from "./MotorcycleProcessor";
-export * from "./LgvProcessor";
-export * from "./PsvProcessor";
-export * from "./TrailerProcessor";
-export * from "./HgvProcessor";
--- src/handlers/index.ts
+++
@@ -1,5 +0,0 @@
-export * from "./AuditDetailsHandler";
-export * from "./ErrorHandler";
-export * from "./NumberGenerator";
-export * from "./TechRecordStatusHandler";
-export * from "./TechRecordsListHandler";
