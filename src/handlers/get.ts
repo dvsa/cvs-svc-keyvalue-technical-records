03c9a2e103aa25b9e6427d505e5f275dca193c6c
Steven.Pears <steven.pears@live.co.uk>
2022-11-15 09:30:31 +0000

    linting

--- src/handlers/get.ts
+++ src/handlers/get.ts
@@ -1,35 +1,35 @@
-import * as AWS from 'aws-sdk';
+import * as AWS from "aws-sdk";
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
-import * as UTIL from '../util'
+import * as UTIL from "../util";
 const client = new AWS.DynamoDB.DocumentClient();
 
-exports.handler = async (event: APIGatewayProxyEvent):Promise<APIGatewayProxyResult> => {
-    //Get & Set Parameters
-    const pathParams = event.pathParameters!
-    const queryParams = event.queryStringParameters!
-    const status = queryParams['status'] as TECH_STATUS_CODE ?? TECH_STATUS_CODE.PROVISIONAL_OVER_CURRENT;
-    const vehicleId = pathParams['vehicleId'];
+exports.handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+    // Get & Set Parameters
+    const pathParams = event.pathParameters!;
+    const queryParams = event.queryStringParameters!;
+    const status = queryParams.status as TECH_STATUS_CODE ?? TECH_STATUS_CODE.PROVISIONAL_OVER_CURRENT;
+    const vehicleId = pathParams.vehicleId;
 
     const result = await client.query({
         TableName: process.env.target_table!,
-        KeyConditionExpression: 'systemNumber = :vehicleId',
-        FilterExpression: 'CONTAINS(status, :status)',
+        KeyConditionExpression: "systemNumber = :vehicleId",
+        FilterExpression: "CONTAINS(status, :status)",
         ScanIndexForward: false,
         ExpressionAttributeValues: {
-            ':vehicleId': vehicleId,
-            ':statusList': status
+            ":vehicleId": vehicleId,
+            ":statusList": status
         }
     }).promise();
-    //console.log('Retrieved from Dynamo');
+    // console.log('Retrieved from Dynamo');
 
-    if (result.Items == undefined || result.Items.length === 0) {
+    if (result.Items === undefined || result.Items.length === 0) {
         return {
             statusCode: 404,
-            body: '',
+            body: "",
         };
     }
 
-    //console.log(JSON.stringify());
+    // console.log(JSON.stringify());
     const vehicle = UTIL.unflatten(result.Items);
 
     const response = {
